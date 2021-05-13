5134f391f4c5ceffffab9ab1748a6905be79db17
Geoff Whatley <me@geoffwhatley.com>
2021-05-13 15:00:35 +1000

    refactor(tasks): update remaining 'complete' task status references

--- src/utils/isTaskComplete.ts
+++ src/utils/isTaskComplete.ts
@@ -1,5 +1,5 @@
 import { Task, TaskStatus } from '../types';
 
 export function isTaskComplete(task: Task): boolean {
-  return task.status === TaskStatus.Complete;
+  return task.status === TaskStatus.Completed;
 }
