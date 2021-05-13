ea30cde913afbe9d2fbf120a2d36917f643f34f1
Geoff Whatley <me@geoffwhatley.com>
2021-05-13 14:51:46 +1000

    feat(tasks): add controls for changing the task status filter

---
+++ src/components/TasksFilters.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { useTasksFilter } from '../hooks/useTasksFilter';
+import { StatusFilter } from '../types';
+
+export function TasksFilters() {
+  const { setStatusFilter, filter } = useTasksFilter();
+
+  return (
+    <div>
+      <button
+        disabled={filter.status === StatusFilter.All}
+        onClick={() => setStatusFilter(StatusFilter.All)}
+      >
+        all
+      </button>
+
+      <button
+        disabled={filter.status === StatusFilter.Pending}
+        onClick={() => setStatusFilter(StatusFilter.Pending)}
+      >
+        pending
+      </button>
+
+      <button
+        disabled={filter.status === StatusFilter.Complete}
+        onClick={() => setStatusFilter(StatusFilter.Complete)}
+      >
+        complete
+      </button>
+    </div>
+  );
+}
