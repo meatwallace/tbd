3d96a7a50d4e33c1f235462023117ff983839a6c
Geoff Whatley <me@geoffwhatley.com>
2021-05-13 14:18:22 +1000

    tests: remove erroneous test nesting

--- src/test-utils/createTask.test.ts
+++ src/test-utils/createTask.test.ts
@@ -1,34 +1,32 @@
 import { TaskStatus } from '../types';
 import { createTask } from './createTask';
 
-test('createTask', () => {
-  it('returns a valid task object', () => {
-    const result = createTask();
+it('returns a valid task object', () => {
+  const result = createTask();
 
-    expect.assertions(1);
+  expect.assertions(1);
 
-    expect(result).toStrictEqual({
-      id: expect.any(String),
-      title: expect.any(String),
-      created: expect.any(Date),
-      updated: null,
-      status: TaskStatus.Pending,
-    });
+  expect(result).toStrictEqual({
+    id: expect.any(String),
+    title: expect.any(String),
+    created: expect.any(Date),
+    updated: null,
+    status: TaskStatus.Pending,
   });
+});
 
-  it("can be passed an object that will be used to set the task's properties", () => {
-    const task = {
-      id: 'abcd1234',
-      title: 'A very specific task title',
-      created: new Date(),
-      updated: new Date(),
-      status: TaskStatus.Complete,
-    };
+it("can be passed an object that will be used to set the task's properties", () => {
+  const task = {
+    id: 'abcd1234',
+    title: 'A very specific task title',
+    created: new Date(),
+    updated: new Date(),
+    status: TaskStatus.Complete,
+  };
 
-    const result = createTask(task);
+  const result = createTask(task);
 
-    expect.assertions(1);
+  expect.assertions(1);
 
-    expect(result).toStrictEqual(task);
-  });
+  expect(result).toStrictEqual(task);
 });
